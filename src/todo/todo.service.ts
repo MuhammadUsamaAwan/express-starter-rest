b286915bbbfacf2eeb02831cc8f39d5d843e9f45
Muhammad Usama <underusama@gmail.com>
2023-06-15 00:28:08 +0500

    redis caching

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -4,9 +4,13 @@ import { eq, and } from 'drizzle-orm';
 import db from '@/db';
 import { insertTodoSchema, updateTodoSchema, todos } from '@/schema/todos';
 import { uuidSchema } from '@/schema/common';
+import { useCache, deleteCache } from '@/helper/cache';
 
 export const getTodos = async (req: Request, res: Response) => {
-  const result = await db.select().from(todos).where(eq(todos.userId, req.user.id));
+  const result = await useCache(
+    `todos_${req.user.id}`,
+    async () => await db.select().from(todos).where(eq(todos.userId, req.user.id))
+  );
   return res.status(200).json(result);
 };
 
@@ -16,6 +20,7 @@ export const addTodo = async (req: Request, res: Response) => {
     .insert(todos)
     .values({ id: randomUUID(), userId: req.user.id, ...newTodo })
     .returning();
+  deleteCache(`todos_${req.user.id}`);
   return res.status(201).json(todo);
 };
 
@@ -27,6 +32,7 @@ export const updateTodo = async (req: Request, res: Response) => {
     .set({ ...updateTodo })
     .where(and(eq(todos.id, id), eq(todos.userId, req.user.id)))
     .returning();
+  deleteCache(`todos_${req.user.id}`);
   return res.status(200).json(todo);
 };
 
@@ -36,5 +42,6 @@ export const deleteTodo = async (req: Request, res: Response) => {
     .delete(todos)
     .where(and(eq(todos.id, id), eq(todos.userId, req.user.id)))
     .returning();
+  deleteCache(`todos_${req.user.id}`);
   return res.status(200).json(todo);
 };
